43152e69bd629a024999de56785bdcb034292a9a
avilash <avilashkumar4@gmail.com>
2019-06-10 20:18:54 +0530

    Normalise ouptuts for MNIST model

--- model/embedding.py
+++ model/embedding.py
@@ -53,4 +53,5 @@ class EmbeddingLeNet(nn.Module):
         output = self.convnet(x)
         output = output.view(output.size()[0], -1)
         output = self.fc(output)
+        output = F.normalize(output, p=2, dim=1)
         return output
